0514d9351bbc98a8decdeeebb0a10701b449872e
Arif <ez.boy763@gmail.com>
2022-07-23 17:15:32 +0600

    wallet height fix

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -25,9 +25,9 @@ function Header() {
 
       <Grid item xs={3} className='headerMenu'>
         <ul style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px', alignItems: 'center', marginTop: '45px' }}>
-          <li><img style={{ padding: '12px' }} src={icon1} /></li>
-          <li><img style={{ padding: '10px' }}  src={icon2} /></li>
-          <li style={{ display: 'flex', width: '120px', alignItems: 'center', gap: '5px' }}><img style={{width: '35px', border: '1px solid #fff', borderRadius: '7px' }} src={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/156905/profile/profile-512.jpg?1530296477'} /> John Doe</li>
+          <li><img alt='icon-menu-1' style={{ padding: '12px' }} src={icon1} /></li>
+          <li><img alt='icon-menu-1' style={{ padding: '10px' }}  src={icon2} /></li>
+          <li style={{ display: 'flex', width: '120px', alignItems: 'center', gap: '5px' }}><img alt='user-img' style={{width: '35px', border: '1px solid #fff', borderRadius: '7px' }} src={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/156905/profile/profile-512.jpg?1530296477'} /> John Doe</li>
         </ul>
       </Grid>
     </Grid>
