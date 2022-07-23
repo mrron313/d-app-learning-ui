95257f2c28851cbd4a1149a6bacb336cb3bdb443
Arif <ez.boy763@gmail.com>
2022-07-23 17:11:26 +0600

    icons sidebar

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,4 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
+import MenuIcon1 from "../../assets/brick-icon.png";
+import MenuIcon2 from "../../assets/book-icon.png";
+import MenuIcon3 from "../../assets/envelope-icon.png";
+import MenuIcon4 from "../../assets/bank-icon.png";
+import MenuIcon5 from "../../assets/wallet-icon.png";
 
 function SideBar() {
   const location = useLocation();
@@ -6,10 +11,11 @@ function SideBar() {
   return (
     <div className="sidebar">
       <ul>
-        <li className={location.pathname === '/creations'? 'activeSidebarMenu' : ''}><Link  to='/creations'>Creations</Link></li>
-        <li className={location.pathname === '/invitation'? 'activeSidebarMenu' : ''} ><Link to='/invitation'>Invitation</Link></li>
-        <li className={location.pathname === '/court'? 'activeSidebarMenu' : ''} ><Link to='/court'>Court</Link></li>
-        <li className={location.pathname === '/wallet'? 'activeSidebarMenu' : ''} ><Link to='/wallet'>Wallet</Link></li>
+        <li className={location.pathname === '/creations'? 'activeSidebarMenu' : ''}><Link  to='/creations'><img alt="menu-icon" src={MenuIcon1} /> Course </Link></li>
+        <li className={location.pathname === '/creations'? 'activeSidebarMenu' : ''}><Link  to='/creations'><img alt="menu-icon" src={MenuIcon2} /> Creations</Link></li>
+        <li className={location.pathname === '/invitation'? 'activeSidebarMenu' : ''} ><Link to='/invitation'><img alt="menu-icon" src={MenuIcon3} />Invitation</Link></li>
+        <li className={location.pathname === '/court'? 'activeSidebarMenu' : ''} ><Link to='/court'><img alt="menu-icon" src={MenuIcon4} /> Litigation</Link></li>
+        <li className={location.pathname === '/wallet'? 'activeSidebarMenu' : ''} ><Link to='/wallet'><img alt="menu-icon" src={MenuIcon5} /> Wallet</Link></li>
       </ul>
     </div>
   );
