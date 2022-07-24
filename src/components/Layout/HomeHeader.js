9faad44e95d2adc3694d0d0e885953a84f8a1c3a
Arif <ez.boy763@gmail.com>
2022-07-25 00:49:53 +0600

    menus fix

--- src/components/Layout/HomeHeader.js
+++ src/components/Layout/HomeHeader.js
@@ -36,11 +36,12 @@ function HomeHeader() {
         </div>
       </Grid>
 
+
       <Grid item xs={3} className='headerMenu'>
-        <ul style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px', alignItems: 'center', marginTop: '45px' }}>
-          <li><img style={{ padding: '12px' }} src={icon1} /></li>
-          <li><img style={{ padding: '10px' }}  src={icon2} /></li>
-          <li style={{ display: 'flex', width: '120px', alignItems: 'center', gap: '5px' }}><img style={{width: '35px', border: '1px solid #fff', borderRadius: '7px' }} src={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/156905/profile/profile-512.jpg?1530296477'} /> John Doe</li>
+        <ul style={{ display: 'flex', justifyContent: 'flex-end', gap: '25px', alignItems: 'center', marginTop: '45px' }}>
+          <li className='menuBox'><img alt='icon-menu-1' src={icon1} /></li>
+          <li className='menuBox'><img alt='icon-menu-1' src={icon2} /></li>
+          <li style={{ display: 'flex', width: '120px', alignItems: 'center', gap: '5px' }}><img alt='user-img' style={{width: '40px', border: '1px solid #fff', borderRadius: '7px' }} src={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/156905/profile/profile-512.jpg?1530296477'} /> John Doe</li>
         </ul>
       </Grid>
     </Grid>
