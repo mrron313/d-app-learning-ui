53a3c185b5cc54d914fe43a6614e66e3f08f3bec
Arif <ez.boy763@gmail.com>
2022-07-26 11:29:21 +0600

    menu active icon

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,24 @@
 import { Grid } from "@mui/material";
+import FacebookIcon from '../../assets/facebook2.png';
+import InstagramIcon from '../../assets/instagram2.png';
+import YoutubeIcon from '../../assets/youtube2.png';
+import TwitterIcon from '../../assets/twitter2.png';
+import LinkedinIcon from '../../assets/linkedin2.png';
 
 function Footer() {
   return ( 
     <Grid container className="footerSection">
-      <Grid item xs={12}>
+      <Grid item xs={6}>
         <p className="footerText">Copyright © 2022. Decentralized e-learning. All Rights Reserved</p>
+      </Grid>
+      <Grid item xs={6} className="socialIcons" justifyContent='flex-end'>
+        <div className='creditCardIcons'>
+          <img src={FacebookIcon} />
+          <img src={TwitterIcon} />
+          <img src={LinkedinIcon} />
+          <img src={InstagramIcon} />
+          <img src={YoutubeIcon} />
+        </div>
       </Grid>
     </Grid>
   );
--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -6,6 +6,13 @@ import MenuIcon4 from "../../assets/bank-icon.png";
 import MenuIcon5 from "../../assets/wallet-icon.png";
 import MenuIcon6 from "../../assets/credit.png";
 
+import MenuIcon1Active from "../../assets/brick-icon-2.png";
+import MenuIcon2Active from "../../assets/book-icon-2.png";
+import MenuIcon3Active from "../../assets/envelope-icon-2.png";
+import MenuIcon4Active from "../../assets/bank-icon-2.png";
+import MenuIcon5Active from "../../assets/wallet-icon-2.png";
+import MenuIcon6Active from "../../assets/credit-icon-2.png";
+
 function SideBar() {
   const location = useLocation();
 
@@ -14,19 +21,29 @@ function SideBar() {
       <ul>
         {/* <li className={location.pathname === '/course'? 'activeSidebarMenu' : ''}><Link  to='/course'><img alt="menu-icon" src={MenuIcon1} /> Course </Link></li> */}
         <li className={location.pathname === '/creations'? 'activeSidebarMenu' : ''}>
-          <Link  to='/creations'><img alt="menu-icon" src={MenuIcon2} /> <span>Creations</span> </Link>
+          <Link  to='/creations'>
+            <img alt="menu-icon" src={location.pathname === '/creations' ? MenuIcon2Active : MenuIcon2} /> <span>Creations</span> 
+          </Link>
         </li>
         <li className={location.pathname === '/invitation'? 'activeSidebarMenu' : ''} >
-          <Link to='/invitation'><img alt="menu-icon" src={MenuIcon3} /><span>Invitation</span></Link>
+          <Link to='/invitation'>
+            <img alt="menu-icon" src={location.pathname === '/invitation' ? MenuIcon3Active : MenuIcon3} /><span>Invitation</span>
+          </Link>
         </li>
         <li className={location.pathname === '/litigation'? 'activeSidebarMenu' : ''} >
-          <Link to='/litigation'><img alt="menu-icon" src={MenuIcon4} /> <span>Litigation</span></Link>
+          <Link to='/litigation'>
+            <img alt="menu-icon" src={location.pathname === '/litigation' ? MenuIcon4Active : MenuIcon4} /> <span>Litigation</span>
+          </Link>
         </li>
         <li className={location.pathname === '/wallet'? 'activeSidebarMenu' : ''} >
-          <Link to='/wallet'><img alt="menu-icon" src={MenuIcon5} />  <span>Wallet</span> </Link>
+          <Link to='/wallet'>
+            <img alt="menu-icon" src={location.pathname === '/wallet' ? MenuIcon5Active : MenuIcon5}  />  <span>Wallet</span> 
+          </Link>
         </li>
         <li className={location.pathname === '/credit'? 'activeSidebarMenu' : ''} >
-          <Link to='/credit'><img alt="menu-icon" src={MenuIcon6} />  <span>Credit</span> </Link>
+          <Link to='/credit'>
+            <img alt="menu-icon" src={location.pathname === '/credit' ? MenuIcon6Active : MenuIcon6}  />  <span>Credit</span> 
+          </Link>
         </li>
       </ul>
     </div>
