ebec917b7a0f17dc194761cb3e75019705981320
Arif <ez.boy763@gmail.com>
2022-08-05 01:43:49 +0600

    credit page and menu

--- src/components/Credit/index.js
+++ src/components/Credit/index.js
@@ -7,12 +7,12 @@ const Credit = () => {
   return ( 
     <Grid container spacing={2}>
 
-        <Grid item xs={9} className='topContributor'>
-            <Grid item xs={5}>
+        <Grid item md={9} xs={12} className='topContributor'>
+            <Grid item md={12}>
                 <Typography className='inviationHeaderTitle' variant='h6'>Top Contributors</Typography>
             </Grid>
 
-            <Grid item xs={12}  className='topContributorSection'>
+            <Grid item md={12} className='topContributorSection'>
                 {[1, 2, 3, 4, 5, 6, 7, 8, ].map((singleCredit) => {
                     return (
                         <CreditCard />
@@ -21,12 +21,12 @@ const Credit = () => {
             </Grid>
         </Grid>
 
-        <Grid item xs={3}>
-            <Grid item xs={12}>
+        <Grid item md={3} xs={12}>
+            <Grid item md={12}>
                 <Typography className='inviationHeaderTitle' variant='h6'>Others Contributors</Typography>
             </Grid>
 
-            <Grid item xs={12}  className='otherContributorSection'>
+            <Grid item md={12}  className='otherContributorSection'>
                 {[1, 2, 3, 4, 5, 6, 7, 8, ].map((singleCredit) => {
                     return (
                         <OtherContributerCard />
