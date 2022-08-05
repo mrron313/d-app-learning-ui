4d74272b6a6c7d51dd59824ff0dbc30da33b1524
Arif <ez.boy763@gmail.com>
2022-08-05 19:30:27 +0600

    fixes

--- src/components/Creations/index.js
+++ src/components/Creations/index.js
@@ -1,4 +1,4 @@
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import CollectionCard from './CollectionCard';
